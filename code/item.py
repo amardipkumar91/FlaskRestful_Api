636b8e1b78525215535edd38f7136cfd942afc76
Amardip Kumar <amardip.kumar@ndi-mb-005.local>
2020-04-08 16:04:39 +0530

    changes

--- code/item.py
+++ code/item.py
@@ -15,6 +15,13 @@ class Item(Resource):
         # for item in items:
         #     if item['name'] == name:
         #         return item
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return ({'message' : 'Item Not Found'}), 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('store.db')
         cursor = connection.cursor()
         query = "SELECT * from items WHERE name = ?"
@@ -23,40 +30,78 @@ class Item(Resource):
         connection.close()
         if row:
             return ({'item': {'name' : row[0], 'price' : row[1] }})
-        return ({'message' : 'Item Not Found'}), 404
+
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
 
     @jwt_required()
     def post(self, name):
-
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return ({'message' : 'Item name with "{}" already exists'.format(name)}), 400 # bad request
             
         data = Item.parser.parse_args()
         item = { 'name' : name, 'price' : data['price']}
-        items.append(item)
+        try:
+            self.insert(item)
+        except Exception as e:
+            return ({'message': 'An error occurred inserting the data'}), 500
         return item, 201
 
     @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM items WHERE name = ?"
+        cursor.execute(query, (name, ))
+        connection.commit()
+        connection.close()
+
         return ({'message' : "Item Deleted"})
+
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+        query = "UPDATE items SET price = ? WHERE name = ?"
+        cursor.execute(query, (item['price'], item['name']))
+        connection.commit()
+        connection.close()
 
     @jwt_required()
     def put(self, name):
-        #request parser
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
         data = Item.parser.parse_args()
-        if item is None:
-            item = {'name' : name, 'price' : data['price']}
-            items.append(item)
-        else:
-            item.update(data)
+        updated_item = { 'name' : name, 'price' : data['price']}
 
-        return item            
+        if self.find_by_name(name):
+            try:
+                self.update(updated_item)
+            except:
+                return ({'message': 'An error occurred updating the data'}), 500
+        else:
+            try:
+                self.insert(updated_item)
+            except:
+                return ({'message': 'An error occurred inserting the data'}), 500
+        return updated_item            
         
 class ItemList(Resource):
     @jwt_required()
     def get(self):
-        return {'items' : items}
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+        query = "SELECT * from items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price' : row[1]})
+        connection.commit()
+        connection.close()
+        return {'items': items}
+        
