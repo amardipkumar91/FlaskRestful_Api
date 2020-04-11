b9560f8d6941215dd2e6060c07956674debc7696
Amardip Kumar <amardip.kumar@ndi-mb-005.local>
2020-04-11 13:27:50 +0530

    ass postgrace

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
@@ -7,11 +8,16 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(['DATABASE_URL'], 'sqlite:///store.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "123456"
 api = Api(app)
+
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity) #/auth
 
